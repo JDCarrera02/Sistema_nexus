8e17b7128ad54cec19ae9ab2091eeef7e27e2516
Juan Diego <jcarreral@iesch.org>
2026-05-21 01:09:11 +0200

    cambios pequeños LoginFrame

--- src/main/java/View/LoginFrame.java
+++ src/main/java/View/LoginFrame.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class LoginFrame extends JFrame {
-    private SesionController sesionController;
+    private final SesionController sesionController;
 
     // Componentes
     private JTextField txtUsuario;
